4d9b1d534247f6b3387c8286bf13c1fadaa62dd1
Marie-Michèle Poulin <marie-michele.poulin@mathben.tech>
2019-09-30 14:52:57 -0400

    [FIX] hr_expense_tip: add French po and fix English syntax

--- hr_expense_tip/models/res_config_settings.py
+++ hr_expense_tip/models/res_config_settings.py
@@ -7,5 +7,5 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     refund_total_tip_amount_included_to_employee = fields.Boolean(
-        string='Add tip when refund employee the expense.',
+        string='Add tip when refunding the employee expenses.',
         config_parameter='hr_expense.refund_total_tip_amount_included_to_employee')
