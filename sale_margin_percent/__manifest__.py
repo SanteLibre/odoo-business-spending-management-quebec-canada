c02d3f155c45e6291721b03c41d5c652df7049d1
Mathieu Benoit <mathieu.benoit@mathben.tech>
2019-10-31 16:07:23 -0400

    [ADD] sale_margin_percent to show percent of margin in sale order line

---
+++ sale_margin_percent/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Margins percent in Sales Orders Line',
+    'version': '1.0',
+    'category': 'Sales',
+    'description': """
+This module adds the 'Margin Percent' on sales order.
+=====================================================
+
+This gives the profitability by calculating the difference between the Unit
+Price and Cost Price.
+    """,
+    'depends': ['sale_management', 'sale_margin'],
+    'data': ['views/sale_margin_view.xml'],
+}
