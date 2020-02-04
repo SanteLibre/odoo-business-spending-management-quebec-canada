fe112303f780d6c3f0a5fe765f5730a1cd5f06ed
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-02-04 00:44:20 -0500

    [IMP] product_lump_sum: add product lump sump and sort it in sale

---
+++ product_lump_sum/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Product Lump sum',
+    'version': '1.0',
+    'category': 'Sales',
+    'description': """
+Product Lump sum
+================
+
+    """,
+    'depends': ['product'],
+    'data': ['data/product_data.xml'],
+}
