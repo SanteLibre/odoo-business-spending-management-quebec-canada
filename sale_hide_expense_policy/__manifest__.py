9397f5618c037ae684cf0dba81ff3774a210c5dc
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-02-04 16:37:14 -0500

    [IMP] sale_hide_expense_policy: hide expense policy

---
+++ sale_hide_expense_policy/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Sale hide expense policy',
+    'version': '0.1',
+    'category': 'Sales',
+    'summary': 'Sale hide expense policy',
+    'description': """
+Sale hide expense policy
+========================
+    """,
+    'depends': ['sale'],
+    'data': [
+        'views/sale_views.xml'
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False
+}
