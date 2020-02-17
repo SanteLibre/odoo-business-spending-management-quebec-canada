c088d30660bc963ba42436cbc03d50e16d144f93
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-02-17 18:09:43 -0500

    [FIX] sale_fix_update_price_unit_when_update_qty - Restore previous price_unit when update only qty

---
+++ sale_fix_update_price_unit_when_update_qty/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sale Fix Update Price Unit When Update QTY',
+    'version': '1.0.0',
+    'author': "MathBenTech",
+    'website': 'https://mathben.tech',
+    'license': 'AGPL-3',
+    'category': 'Sales',
+    'summary': 'Sale Fix Update Price Unit When Update QTY',
+    'description': """
+Sale Fix Update Price Unit When Update QTY
+==========================================
+
+""",
+    'depends': ['sale'],
+    'data': [
+    ],
+    'installable': True,
+}
