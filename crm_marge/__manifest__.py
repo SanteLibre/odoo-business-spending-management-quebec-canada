c2228206cde88b05432e9f17b3843a5b07b8e840
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-02-20 02:54:37 -0500

    [IMP] crm_marge: support marge, cost, markup and marge_percent - Add it in each view - planned_revenu and markup are readonly

---
+++ crm_marge/__manifest__.py
@@ -0,0 +1,23 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'CRM marge',
+    'version': '12.0.0.0.1',
+    'author': "MathBenTech",
+    'website': 'https://mathben.tech',
+    'license': 'AGPL-3',
+    'category': 'Customer Relationship Management',
+    'summary': 'CRM marge',
+    'description': """
+CRM marge
+=========
+
+""",
+    'depends': [
+        'crm'
+    ],
+    'data': [
+        'views/crm_lead_view.xml',
+    ],
+    'installable': True,
+}
