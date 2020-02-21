5819a8fe51b942ddeaeb785e6ffeac80a9437770
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-02-21 16:54:56 -0500

    [IMP] menu_force_project_first - always move menu project to first sequence

---
+++ menu_force_project_first/models/ir_ui_menu.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, fields, models
+
+
+class IrUiMenu(models.Model):
+    _inherit = "ir.ui.menu"
+
+    @api.multi
+    def write(self, values):
+        if (values.get("parent_id") is False and values.get("name") == "Project") or (
+                self.name == _("Project") and self.parent_id.id is False):
+            values["sequence"] = -10
+
+        return super(IrUiMenu, self).write(values)
