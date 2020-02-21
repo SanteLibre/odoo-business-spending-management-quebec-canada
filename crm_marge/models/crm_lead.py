e4b6cb994a872bfc792945a7c1ad35622065a0e6
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-02-21 14:11:56 -0500

    clean wip

--- crm_marge/models/crm_lead.py
+++ crm_marge/models/crm_lead.py
@@ -12,16 +12,6 @@ class CrmMarge(models.Model):
     marge = fields.Monetary(string="Marge", currency_field='company_currency')
     marge_percent = fields.Float(string="Marge %")
 
-    # def is_minimal_2_field_set(self):
-    #     """
-    #
-    #     :return: true if 2 field is set
-    #     """
-    #     lst_field_1 = [a for a in (self.cost, self.marge) if a]
-    #     lst_field_2 = [a for a in (self.cost, self.marge_percent) if a]
-    #
-    #     return len(lst_field_1) > 1 or len(lst_field_2) > 1
-
     @api.multi
     @api.onchange('marge', 'cost')
     def onchange_marge(self):
@@ -45,27 +35,8 @@ class CrmMarge(models.Model):
         Influence marge and planned_revenue
         """
         for lead in self:
-            # lead.planned_revenue = lead.marge + lead.cost
             if lead.marge_percent >= 100.:
                 lead.marge_percent = 99.999999
 
             lead.marge = (lead.cost / (1 - (lead.marge_percent / 100))) - lead.cost
             lead.planned_revenue = lead.marge + lead.cost
-
-            # lead.marge = lead.planned_revenue * (lead.marge_percent / 100.)
-            # lead.cost = lead.planned_revenue - lead.marge
-            #
-            # if lead.planned_revenue:
-            #     lead.marge = 100. * lead.marge / lead.planned_revenue
-            # else:
-            #     lead.marge_percent = 0.
-
-    # @api.multi
-    # @api.onchange('cost')
-    # def onchange_cost(self):
-    #     """
-    #     Update cost
-    #     Influence planned_revenue
-    #     """
-    #     for lead in self:
-    #         lead.planned_revenue = lead.marge + lead.cost
