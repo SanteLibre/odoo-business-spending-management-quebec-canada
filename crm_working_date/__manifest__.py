86390182909260303ec199e78e837469d182b141
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-02-21 22:21:26 -0500

    [IMP] crm_working_date: Add fields start_date, due_date and duration This field can be use of project.

---
+++ crm_working_date/__manifest__.py
@@ -0,0 +1,23 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'CRM working date',
+    'version': '12.0.0.0.1',
+    'author': "MathBenTech",
+    'website': 'https://mathben.tech',
+    'license': 'AGPL-3',
+    'category': 'Customer Relationship Management',
+    'summary': 'CRM working date',
+    'description': """
+CRM working date
+================
+
+""",
+    'depends': [
+        'crm'
+    ],
+    'data': [
+        'views/crm_lead_view.xml',
+    ],
+    'installable': True,
+}
