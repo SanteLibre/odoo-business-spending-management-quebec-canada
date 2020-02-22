0d9d0ac55993024fa6040207c619452c249bfcea
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-02-22 15:19:45 -0500

    Fix sale_fix_update_price_unit_when_update_qty

--- sale_fix_update_price_unit_when_update_qty/models/sale.py
+++ sale_fix_update_price_unit_when_update_qty/models/sale.py
@@ -10,6 +10,5 @@ class SaleOrderLine(models.Model):
     def product_uom_change(self):
         previous_price_unit = self.price_unit
         status = super(SaleOrderLine, self).product_uom_change()
-        if self.price_unit:
-            self.price_unit = previous_price_unit
+        self.price_unit = previous_price_unit
         return status
