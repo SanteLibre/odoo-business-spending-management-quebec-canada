b42b6caa4643614a6834bada50263c2e25e627c9
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-02-21 23:26:38 -0500

    Fix last commit crm_marge Readonly field need compute

--- crm_marge/models/crm_lead.py
+++ crm_marge/models/crm_lead.py
@@ -7,12 +7,26 @@ class CrmMarge(models.Model):
     _inherit = "crm.lead"
 
     planned_revenue = fields.Monetary('Expected Revenue', currency_field='company_currency', track_visibility='always',
-                                      readonly=True)
+                                      compute='_compute_planned_revenue', readonly=True)
     cost = fields.Monetary(string="Cost", currency_field='company_currency')
     gross_margin = fields.Monetary(string="Gross margin", currency_field='company_currency')
     gross_margin_percent = fields.Float(string="Gross margin %")
-    markup_percent = fields.Float(string="Markup %", track_visibility='always',
-                             readonly=True)
+    markup_percent = fields.Float(string="Markup %", compute='_compute_markup_percent', readonly=True)
+
+    @api.multi
+    @api.depends('gross_margin', 'cost')
+    def _compute_markup_percent(self):
+        for lead in self:
+            if lead.cost:
+                lead.markup_percent = 100. * lead.gross_margin / lead.cost
+            else:
+                lead.markup_percent = 0.
+
+    @api.multi
+    @api.depends('gross_margin', 'cost')
+    def _compute_planned_revenue(self):
+        for lead in self:
+            lead.planned_revenue = lead.gross_margin + lead.cost
 
     @api.multi
     @api.onchange('gross_margin', 'cost')
@@ -22,17 +36,10 @@ class CrmMarge(models.Model):
         Influence gross_margin_percent and planned_revenue
         """
         for lead in self:
-            lead.planned_revenue = lead.gross_margin + lead.cost
-
             if lead.planned_revenue:
                 lead.gross_margin_percent = 100. * lead.gross_margin / lead.planned_revenue
-                if lead.cost != 0:
-                    lead.markup_percent = 100. * lead.gross_margin / lead.cost
             else:
                 lead.gross_margin_percent = 0.
-                lead.markup_percent = 0.
-
-
 
     @api.multi
     @api.onchange('gross_margin_percent')
@@ -46,7 +53,3 @@ class CrmMarge(models.Model):
                 lead.gross_margin_percent = 99.999999
 
             lead.gross_margin = (lead.cost / (1 - (lead.gross_margin_percent / 100))) - lead.cost
-            lead.planned_revenue = lead.gross_margin + lead.cost
-            if lead.cost != 0:
-                lead.markup_percent = 100. * lead.gross_margin / lead.cost
-
