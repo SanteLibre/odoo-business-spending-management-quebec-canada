44ab2b3041eb8d643f01400b6c234461b7cc55ee
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-02-21 22:21:26 -0500

    [IMP] crm_working_date: Add fields start_date, due_date and duration This field can be use of project.

---
+++ crm_working_date/models/crm_lead.py
@@ -0,0 +1,31 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, fields, models
+
+
+class Lead(models.Model):
+    _inherit = "crm.lead"
+
+    due_date = fields.Datetime(string="Due Date", help="Add the due date of the project.")
+    start_date = fields.Datetime(string="Start Date", help="Add start date of the project.")
+    duration_estimate_hour = fields.Integer(string="Duration Estimate Hour",
+                                            help="The duration estimated in hour of the project.")
+
+    @api.multi
+    @api.onchange('due_date', 'start_date')
+    def onchange_due_start_date(self):
+        """
+        Check if due_date is after start_date
+        """
+        for lead in self:
+            warning = {}
+            if lead.due_date and lead.start_date:
+                # Need to support the same day
+                if lead.start_date > lead.due_date:
+                    warning = {
+                        'title': "Warning date error",
+                        'message': "The due date is before the start date.",
+                    }
+
+            if warning:
+                return {"warning": warning}
