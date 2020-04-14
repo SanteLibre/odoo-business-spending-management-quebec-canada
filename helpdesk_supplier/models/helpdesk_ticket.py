ed32779e5277c04ac01f82ea22caf0651d4fb233
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-04-13 23:13:37 -0400

    [IMP] helpdesk: rework all module and update dependancy

--- helpdesk_supplier/models/helpdesk_ticket.py
+++ helpdesk_supplier/models/helpdesk_ticket.py
@@ -5,45 +5,38 @@ from odoo.exceptions import UserError
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
+    show_create_supplier = fields.Boolean(compute="_compute_show_create_supplier",
+                                          readonly=True)
+
+    @api.multi
+    @api.depends('category_id', 'partner_id')
+    def _compute_show_create_supplier(self):
+        data_category = self.env.ref(
+            'helpdesk_supplier.helpdesk_ticket_category_supplier_applicant').id
+        for val in self:
+            if data_category == val.category_id.id:
+                val.show_create_supplier = not val.partner_id or (
+                            val.partner_id and not val.partner_id.supplier)
+
     def create_supplier(self):
-        if not self.partner_name:
-            raise UserError(_('The partner name need to be filled.'))
-
-        if not self.partner_email:
-            raise UserError(_('The partner email need to be filled.'))
-
-        # TODO use reference
-        if self.category_id.id != 4:
-            raise UserError(_('The category need to be "Joindre fournisseur".'))
-
-        values = {
-            "name": self.partner_name,
-            "supplier": True,
-            "customer": False,
-            "street": self.partner_address,
-            "email": self.partner_email,
-            "phone": self.partner_phone,
-        }
-
-        partner_id = self.env['res.partner'].create(values)
-        self.partner_id = partner_id.id
+        if not self.partner_id:
+            self.create_res_partner(is_supplier=True)
+        else:
+            self.partner_id.supplier = True
 
     def send_supplier_applicant_mail(self):
         if self.partner_email:
-            self.env.ref('helpdesk_supplier.assignment_supplier_applicant_email_template'). \
+            self.env.ref(
+                'helpdesk_supplier.assignment_supplier_applicant_email_template'). \
                 send_mail(self.id, email_values={}, force_send=True)
 
-    def create(self, vals):
-        # if vals.get('number', '/') == '/':
-        #     seq = self.env['ir.sequence']
-        #     if 'company_id' in vals:
-        #         seq = seq.with_context(force_company=vals['company_id'])
-        #     vals['number'] = seq.next_by_code(
-        #         'helpdesk.ticket.sequence') or '/'
-        res = super().create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        res_list = super(HelpdeskTicket, self).create(vals_list)
 
-        if res and vals.get('category_id') == self.env.ref(
-                'helpdesk_supplier.helpdesk_ticket_category_supplier_applicant').id:
-            res.send_supplier_applicant_mail()
+        for res in res_list:
+            if res.category_id.id == self.env.ref(
+                    'helpdesk_supplier.helpdesk_ticket_category_supplier_applicant').id:
+                res.send_supplier_applicant_mail()
 
         return res
